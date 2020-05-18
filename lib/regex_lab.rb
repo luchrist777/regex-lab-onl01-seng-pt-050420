dee719e17d7cb17a164f14c299e64aa6b10b39e0
Luis Leandro <luchrist777@gmail.com>
2020-05-18 22:36:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,35 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
-  if word.match(/^[AEIOU]|^[aeiou]/)
-    true
+# (/[aeiou]/)
+
+  if word.match(/\A[aeiou]/i) == nil
+    return false
   else
-    false
+    return true
   end
 end
-#word_1 = "afoot"
-#word_2 = "chandoo"
-#starts_with_a_vowel?(word_2)
 
 def words_starting_with_un_and_ending_with_ing(text)
-#returns an array with the words starting with 'un' and ending with 'ing'
-  text.scan(/un\w+ing/)
-  #text.scan(/un\w+ing\b/)
+  text.scan(/\bun\w+ing\b/)
+end
+
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_phone_number?(phone)
+  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
+    return false
+  else
+    return true
+  end
 end
