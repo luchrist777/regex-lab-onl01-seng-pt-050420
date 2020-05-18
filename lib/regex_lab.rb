74dbadad06416530aa5ce408db561ed5b72c49ec
Luis Leandro <luchrist777@gmail.com>
2020-05-18 22:09:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,16 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AEIOU]|^[aeiou]/)
+    true
+  else
+    false
+  end
 end
+#word_1 = "afoot"
+#word_2 = "chandoo"
+#starts_with_a_vowel?(word_2)
 
 def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
+#returns an array with the words starting with 'un' and ending with 'ing'
+  text.scan(/un\w+ing/)
+  #text.scan(/un\w+ing\b/)
 end
